f5a9e062253859afbee6c74a0e652bb44aee774e
Philippe <philippe2008@hotmail.com>
2019-03-03 18:55:09 -0500

    The dev environment issue with React and VS2015.

---
+++ ReactAspx/Content/src/food.js
@@ -0,0 +1,9 @@
+// A '.tsx' file enables JSX support in the TypeScript compiler, 
+// for more information see the following page on the TypeScript wiki:
+// https://github.com/Microsoft/TypeScript/wiki/JSX
+"use strict";
+var React = require("react");
+var ReactDOM = require("react-dom");
+ReactDOM.render(<div>
+        <h2>Test</h2>
+    </div>, document.getElementById("foodorder"));
