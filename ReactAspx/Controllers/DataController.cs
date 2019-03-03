58c92a80a08251ad7d97c6bb68e265d32063b907
Philippe <philippe2008@hotmail.com>
2019-03-03 13:19:15 -0500

    Create a REST API controller.

---
+++ ReactAspx/Controllers/DataController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReactAspx.Controllers
+{
+    public class DataController : Controller
+    {
+        // GET: Data
+        [HttpGet]
+        public ActionResult GetMenuList()
+        {
+            return null;
+        }
+
+        [HttpGet]
+        public string GetUserID()
+        {
+            return "1";
+        }
+
+        [HttpPost]
+        public ActionResult PlaceOrder()
+        {
+            return null;
+        }
+    }
+}
