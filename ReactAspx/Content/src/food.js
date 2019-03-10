8e54eddbb025ec908ed8a342d04f26dac720931e
Philippe <philippe2008@hotmail.com>
2019-03-09 21:54:06 -0500

    Set-Up React, type definitions and configs.

--- ReactAspx/Content/src/food.js
+++ ReactAspx/Content/src/food.js
@@ -1,9 +1,5 @@
-// A '.tsx' file enables JSX support in the TypeScript compiler, 
-// for more information see the following page on the TypeScript wiki:
-// https://github.com/Microsoft/TypeScript/wiki/JSX
 "use strict";
-var React = require("react");
-var ReactDOM = require("react-dom");
-ReactDOM.render(<div>
-        <h2>Test</h2>
-    </div>, document.getElementById("foodorder"));
+const React = require("react");
+const ReactDOM = require("react-dom");
+ReactDOM.render(React.createElement("div", null, React.createElement("h2", null, "Test")), document.getElementById("foodorder"));
+//# sourceMappingURL=food.js.map
