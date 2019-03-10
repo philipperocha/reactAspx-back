368c8fa138fb03af77942b80ff761ae4ad1f7987
Philippe <philippe2008@hotmail.com>
2019-03-10 13:43:31 -0500

    Add a React Component and fetch data.

---
+++ ReactAspx/Content/src/MenuBox.tsx
@@ -0,0 +1,42 @@
+﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
+// for more information see the following page on the TypeScript wiki:
+// https://github.com/Microsoft/TypeScript/wiki/JSX
+
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { FoodModel, IAppState } from "./Models";
+
+export class MenuBox extends React.Component<any, IAppState> {
+    constructor(state) {
+        super(state);
+        this.state = {
+            items: null,
+            myOrder: null,
+            showPopup: null,
+            userId: 0,
+            orderPlaced: false
+        };
+
+        this.loadMenusFromServer();
+    }
+
+    loadMenusFromServer() {
+        var xhr = new XMLHttpRequest();
+        xhr.open('get', '/data/GetMenuList/', true);
+
+        xhr.onload = function () {
+            var dataitems = JSON.parse(xhr.responseText);
+            var tmp: IAppState = this.state;
+            tmp.items = dataitems;
+            this.setState(tmp);
+        }.bind(this);
+
+        xhr.send();
+    }
+
+    render() {
+        return (
+            <div>Text 123</div>
+        );
+    }
+}
